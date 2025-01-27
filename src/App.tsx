a98dba5312a617ff4d38cfcd0a748505749ba393
helloals <helloals5236@naver.com>
2025-01-27 16:48:25 +0900

    about페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+//페이지 컴포넌트
+import MainPage from '@pages/index'
+import AboutPage from "@pages/about"
+
+
+
+
 function App() {
-  return <div>App 컴포넌트 화면</div>
+  return (
+    
+    <BrowserRouter>
+    <Routes>
+      <Route index path="/" element={<MainPage/>}></Route>
+      <Route index path="/about" element={<AboutPage/>}></Route>
+  </Routes>
+  </BrowserRouter>
+)
   
 }
 
